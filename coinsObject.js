f57a7be9d0dfc5181814ccdfdb18379d24d77851
Thomas <thomasmcguire.student@careerdevs.com>
2018-10-11 23:41:52 +0000

    coins

---
+++ coinsObject.js
@@ -0,0 +1,5 @@
+var assignment = "create an object for coins";
+
+var currencyAbbreviations = ["AUD", "GBP", "EUR", "JPY", "CHF", "USD", "AFN", "ALL", "DZD", "AOA"];
+
+var USD = {penny: .01, nickle: .05, dime: .10 , quarter: .25, halfDollar: .50, dollar: 1  };
